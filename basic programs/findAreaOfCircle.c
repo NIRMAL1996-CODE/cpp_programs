5429f7b4e20d1f7f18edbd8cbd02002f2acfce54
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-17 20:35:54 +0530

    write a program to find the area of circle

---
+++ basic programs/findAreaOfCircle.c
@@ -0,0 +1,12 @@
+//write a program to find the area of circle//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r, area;
+    cout<<"ENTER RADIUS= ";
+    cin>>r;
+    area=(float)22/7*r*r;
+    cout<<"AREA OF CIRCLE ="<<area;
+    return 0;
+}
