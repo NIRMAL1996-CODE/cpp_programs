f46d55eb0a9ec830a4195a04c8b1d35886717c5c
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-17 19:52:14 +0530

    write a program to find the sum of n natural number

---
+++ basic programs/findSumOfNaturalNum.cpp
@@ -0,0 +1,12 @@
+//write a program to find the sum of n natural number//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cout<<"ENTER n=";
+    cin>>n;
+    sum= n*(n+1)/2;
+    cout<<"sum is "<<sum;
+    return 0;
+}
