a80184aa8947f42bd2c1a3d43aa780adb104f48c
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-17 13:51:36 +0530

    write a program to add two numbers

---
+++ basic programs/addition.cpp
@@ -0,0 +1,12 @@
+//write a program to add two numbers//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter two number";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition is"<<c;
+    return 0;
+}
