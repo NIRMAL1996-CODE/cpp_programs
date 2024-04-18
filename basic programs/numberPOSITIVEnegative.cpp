04eb1fb173e679d977ac5b762696af06159455aa
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-18 19:31:56 +0530

    write a program to find the whether a number is positive or negative

---
+++ basic programs/numberPOSITIVEnegative.cpp
@@ -0,0 +1,19 @@
+//write a program to find the whether a number is positive or negative//
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout <<" ENTER A NUMBERS ="<< endl;
+    cin>>a;
+    cout<<" NUMBER IS  ";
+    if(a>0)
+    {
+        cout<<"positive";
+    }
+    else
+    {
+        cout<<"negative";
+    }
+    return 0;
+}
