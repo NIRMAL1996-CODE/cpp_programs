2b09393bcb5f9f06b513e1a2ca43a6aa985572b9
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-18 19:42:57 +0530

    Write program to check age of a person

---
+++ basic programs/checkAge.cpp
@@ -0,0 +1,18 @@
+//Write condition to check age of a person//
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout <<" ENTER AGE ="<< endl;
+    cin>>age;
+    if(age>=12 && age<=50)
+    {
+        cout<<"young";
+    }
+    else
+    {
+        cout<<"not young";
+    }
+    return 0;
+}
