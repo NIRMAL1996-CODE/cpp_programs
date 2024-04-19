bda30039ed936b12ba3780a523e783be5dc6b3b6
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-19 20:51:15 +0530

    write aprogram to check that number is palindrome number

---
+++ basic programs/loops/palindrome.cpp
@@ -0,0 +1,22 @@
+//write aprogram to check that number is palindrome number//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,rev=0,r;
+    int num=n;
+    cout<<"ENTER NUMBER=";
+    cin>>n;
+    for(int i=1;n>0;i++)
+    {
+       r=n%10;
+       n=n/10;
+       rev=rev*10+r;
+   }
+   cout<<rev<<" ";
+   if(rev==num)
+   cout<<"its palindrome";
+   else
+   cout<<"its not a palindrome";
+        return 0;
+    }
