dcc07b039c1be539cd700674cd11bce6bd37f9b3
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-25 02:05:20 +0530

    write a program in c++ to find a comparison of strings

---
+++ basic programs/string in c++/compareFunction.cpp
@@ -0,0 +1,11 @@
+//write a program in c++ to find a comparison of strings//
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str1="hello";
+    string str2= "apple";
+    cout<<str1.compare(str2)<<endl;
+    return 0;
+}
