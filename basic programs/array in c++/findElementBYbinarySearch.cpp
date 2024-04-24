91a2a1d19b8fd516f69a6f92565df6396f680a50
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-24 15:57:38 +0530

    write a program to find element of a Array by binary search

---
+++ basic programs/array in c++/findElementBYbinarySearch.cpp
@@ -0,0 +1,38 @@
+//write a program to find element of a Array by binary search//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[]={23,45,78,98,10},key;
+    int size=sizeof(a)/sizeof(a[0]);
+    bool found=false;
+    int low=0,high=size-1,mid=0;
+    cout<<"ENTER KEY ELEMENT = ";
+    cin>>key;
+        while(low<=high)
+        {
+               mid=(low+high)/2;
+                if(key==a[mid])
+                {
+                    found=true;
+                    break;
+                }
+                else if(key<a[mid])
+                       {
+                            high=mid-1;
+                       }
+                       else
+                       {
+                          low=mid+1;
+                       }
+         }
+            if(found==true)
+            {
+                cout<<"KEY ELEMENT IS FOUND"<<endl;
+            }
+            else
+            {
+                cout<<"KEY ELEMENT NOT FOUND "<<endl;
+            }
+    return 0;
+}
