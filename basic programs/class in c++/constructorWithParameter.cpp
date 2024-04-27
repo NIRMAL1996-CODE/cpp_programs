1c577cb6e31150d0f1f562914def52acd7e62e69
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-04-28 01:30:40 +0530

    create constructor of class with its parameters

---
+++ basic programs/class in c++/constructorWithParameter.cpp
@@ -0,0 +1,26 @@
+//create constructor of class with its parameters//
+#include<iostream>
+using namespace std;
+class car
+{
+public:
+    int num;
+    string name;
+    string model;
+    car(int a,string b, string c)
+    {
+        num=a;
+        name=b;
+        model=c;
+    }
+};
+
+int main()
+{
+    car obj1(10,"BMW","XX");
+    car obj2(20,"FORD","ZZ");
+    
+    cout<<obj1.num<<" "<<obj1.name<<" "<<obj1.model<<endl;
+    cout<<obj2.num<<" "<<obj2.name<<" "<<obj2.model<<endl;
+    return 0;
+}
