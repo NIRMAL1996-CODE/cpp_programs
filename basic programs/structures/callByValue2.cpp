b7592073ec7e59527528b34ad240915767c3f473
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-05-06 23:24:48 +0530

    write a program to create a structure as a parameter(call by value)

---
+++ basic programs/structures/callByValue2.cpp
@@ -0,0 +1,32 @@
+//write a program to create a structure as a parameter(call by value)//
+#include<iostream>
+using namespace std;
+//int area(struct rectangle r1);
+void fun(struct rectangle r1);
+struct rectangle
+{
+  int  length;
+   int breadth;
+};
+int main()
+{
+    struct rectangle r={10,20};
+    fun(r);
+   // cout<<"area="<<area(r)<<endl;
+    cout<<"length="<<r.length<<endl<<"breadth="<<r.breadth<<endl;
+    return 0;
+}
+
+/*
+ int area(struct rectangle r1)//call by value //
+{
+    return r1.length* r1.breadth;
+}
+ */
+void fun(struct rectangle r1)// call by value
+{
+    r1.length=20;
+    r1.breadth=69;
+    cout<<"length="<<r1.length<<endl<<"breadth="<<r1.breadth<<endl;
+    
+}
