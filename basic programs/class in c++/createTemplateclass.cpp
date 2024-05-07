d6f042eee9079982db0dba52f14f241cc9b4b20f
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-05-07 23:03:59 +0530

    write a program to create a template class to add and subtract integers

---
+++ basic programs/class in c++/createTemplateclass.cpp
@@ -0,0 +1,44 @@
+//write a program to create a template class to add and subtract integers
+#include<iostream>
+using namespace std;
+template<class T>
+class arithmetic
+{
+private:
+    T a;
+    T b;
+public:
+    arithmetic(T a,T b);
+    int add();
+    int sub();
+};
+template<class T>
+arithmetic<T>::arithmetic(T a,T b)
+{
+    this->a=a;
+    this->b=b;
+}
+template<class T>
+int arithmetic<T> :: add()
+{
+    T c;
+    c= a+b;
+    return c;
+}
+template<class T>
+int arithmetic<T> :: sub()
+{
+    T c;
+    c= a-b;
+    return c;
+}
+
+int main()
+{
+    arithmetic A(20,8);
+    
+    cout<<"Add= "<<A.add()<<endl;
+    cout<<"sub= "<<A.sub()<<endl;
+    return 0;
+    
+}
