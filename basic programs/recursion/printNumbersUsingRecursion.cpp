0c11d9169f780057facc2aad85485fb82aa1036c
NIRMAL JAISWAL <158711380+NIRMAL1996-CODE@users.noreply.github.com>
2024-05-09 15:40:57 +0530

    write a program that uses recursion.

---
+++ basic programs/recursion/printNumbersUsingRecursion.cpp
@@ -0,0 +1,19 @@
+// write a program that uses recursion//
+#include<iostream>
+using namespace std;
+
+void fun1(int n)
+{
+    if(n>0)
+    {
+        cout<<n<<" "<<endl;
+        fun1(n-1);
+    }
+}
+
+int main()
+{
+    int x=10;
+    fun1(x);
+    return 0;
+}
