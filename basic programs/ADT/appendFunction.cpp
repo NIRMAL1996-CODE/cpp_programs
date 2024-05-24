ee2fb8dfa7aabe4a5e5a3a927383b53cc5180734
NIRMAL_JAISWAL <nirmaljaiswal888@gmail.com>
2024-05-24 23:42:14 +0530

    write a program to perform append operations in an array

--- basic programs/ADT/appendFunction.cpp
+++ basic programs/ADT/appendFunction.cpp
@@ -15,14 +15,14 @@ void display(struct array arr)
         printf("%d  \n",arr.A[i]);
 }
 
-void Append(struct array *arr, int x)
+void Append(struct array *arr, int x)// this function is use to add elements at the end of the array//
 {
-    if(arr->length<arr->size)
-        arr->A[arr->length++]=x;
+    if(arr->length<arr->size)//to do so, the length of array always smaller than size of array//
+        arr->A[arr->length++]=x;//by adding new elemets the length also get increased//
 }
 int main()
-{
-    struct array arr={{2,3,4,5,6},10,5};
+{                  // {{elements of array},value,index};                                
+    struct array arr={{2,3,4,5,6},10,5};//initialising array with index on which value will add//
     Append(&arr,10);
     display(arr);
     
