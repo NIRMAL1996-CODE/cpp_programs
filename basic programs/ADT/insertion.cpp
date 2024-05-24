2c651e9c579143cadf21bcdf21147b451ff9a836
NIRMAL_JAISWAL <nirmaljaiswal888@gmail.com>
2024-05-24 23:48:44 +0530

    write a program using insertion operations to insert elements in an array

--- basic programs/ADT/insertion.cpp
+++ basic programs/ADT/insertion.cpp
@@ -1,7 +1,7 @@
 // write a program using insertion operations to insert elements in an array//
 #include<stdio.h>
 #include<stdlib.h>
-struct array
+ struct array
 {
     int A[10];
     int size;
@@ -21,21 +21,21 @@ void Append(struct array *arr, int x)
         arr->A[arr->length++]=x;
 }
 void insert(struct array *arr, int index, int x )
-{
+{//this function is use to add elements anywhere in the array by shifting other elements for freeup the index//
     int i;
     if(index>=0 && index <=arr->length)
     {
         for(i=arr->length;i>index;i--)
-            arr->A[i]=arr->A[i-1];
-        arr->A[index]=x;
-        arr->length++;
+            arr->A[i]=arr->A[i-1];//cpoying and shifting elements using for loop//
+        arr->A[index]=x; //insertion of value on given index//
+        arr->length++; //increment in length//
     
     }
 }
 int main()
 {
     struct array arr={{2,3,4,5,6},10,5};
-    insert(&arr,2,10);
+    insert(&arr,2,10);//syntax-: insert(index,value)//
     display(arr);
     
     return 0;
