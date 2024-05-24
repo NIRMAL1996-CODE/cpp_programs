41211915a10d1694fa468d7bfd2cb308bd89ba89
NIRMAL_JAISWAL <nirmaljaiswal888@gmail.com>
2024-05-24 23:13:48 +0530

    write a program on display operations on an array

--- basic programs/ADT/displayOperation.cpp
+++ basic programs/ADT/displayOperation.cpp
@@ -1,13 +1,13 @@
 // write a program on display operations on an array//
 #include<stdio.h>
 #include<stdlib.h>
-struct array
-{
-    int *A;
-    int size;
-    int length;
+struct array// define a structure for array ADT //
+{   
+    int *A;//pointer for array to create memory in heap//
+    int size;//declare of variable for size of array//
+    int length ;//declare of variable for length of array//
 };
-void display(struct array arr)
+void display(struct array arr)//this function is used to display elements of array//
 {
     int i;
     printf("\n elements are\n");
@@ -19,19 +19,20 @@ int main()
     struct array arr;
     printf("enter size of an array");
     scanf("%d",&arr.size);
-    int i,n;
-    arr.A=(int *)malloc(arr.size*sizeof(int));
+    int i,num;
+    arr.A=(int *)malloc(arr.size*sizeof(int));/*malloc is used to dynamically allocate a block of memory
+    on heap*/
     arr.length=0;
     
     
     printf("enter number of numbers");
-    scanf("%d",&n);
+    scanf("%d",&num);
     
     printf("enter all elements\n");
-        for(i=0;i<n;i++)
+        for(i=0;i<num;i++)
         scanf("%d",&arr.A[i]);
-    arr.length=n;
+    arr.length=num;
     
-    display(arr);
+    display(arr);// display the elements of array//
     return 0;
 }
