9e23c160d5f144759ed5c6030770c1782bb5f8fb
NIRMAL_JAISWAL <nirmaljaiswal888@gmail.com>
2024-05-31 14:17:51 +0530

    create a structure in c++ using encapsulation

--- basic programs/structures/memberFunction.cpp
+++ basic programs/structures/memberFunction.cpp
@@ -1,8 +1,10 @@
-// create a structure in c++ named book with its member variables and its member function//
+// create a structure in c++ using encapsulation//
 #include<iostream>
 using namespace std;
-
+//structures c c does not all encapsulation in it, but this possible with c++//
 struct book
+/*this is call encapsulation, when we bundling data and related functions 
+together while controlling access using access specifiers like public,private, and protected.*/
 {
     int bookid;
     char title[20];
