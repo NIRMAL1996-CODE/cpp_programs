52a126634ae7241f1c75be28d8c7669ff88f9043
NIRMAL_JAISWAL <nirmaljaiswal888@gmail.com>
2024-06-04 17:31:38 +0530

    write program to traversing a single linkedlist and count number of nodes in c

---
+++ basic programs/Linkedlist/traversing.singlelinkedlist.cpp
@@ -0,0 +1,51 @@
+//write program to traversing a single linkedlist and count number of nodes in c//
+#include<stdio.h>
+#include<stdlib.h>
+//to create a node use structure//
+struct node
+{
+    int data; //declare variable to store data //
+    struct node *link;//declare variable to store address//
+};
+//create a function to count number of nodes//
+void countNodes(struct node *head)
+{
+    int count=0;//initialpoint to  count number of nodes//
+    if(head==NULL)//check whether link is null becoz if its null its not pointing to anything//
+    {
+        printf("linkedlist is empty");//if yes print empty//
+    }
+    struct node *p=NULL;//other create a pointer to assign head to it as head is not null it contains value//
+    p=head;//due to this p is now point to 1st value of linkedlist//
+    while(p!=NULL)//we have while loop which run untill p becomes null//
+    {
+        count++;//keeps the number of nodes//
+        p=p->link;//this will always change value of p link//
+    }
+    printf("number of nodes = %d\n",count);//print count//
+}
+
+int main()
+{
+    struct node *head;
+    head =(struct node*)malloc(sizeof(int));
+    head->data =45;
+    head->link=NULL;
+    printf("%d\n", head->data);
+    
+    struct node *head1;
+    head1 =(struct node*)malloc(sizeof(int));
+    head1->data =55;
+    head1->link=NULL;
+    head->link=head1;
+    printf("%d\n", head1->data);
+    
+    head1 =(struct node*)malloc(sizeof(int));
+    head1->data =65;
+    head1->link=NULL;
+    head->link->link=head1;//to link nodes without creating pointer again andd again//
+    printf("%d\n", head1->data);
+    
+    countNodes(head);
+    return 0;
+}
