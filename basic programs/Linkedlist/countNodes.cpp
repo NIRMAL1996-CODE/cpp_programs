dc0dcb1857466b9da50f7c6eabcad3603ea1ef7f
NIRMAL_JAISWAL <nirmaljaiswal888@gmail.com>
2024-06-07 23:29:56 +0530

    write a program to count number of nodes in linkedlist

---
+++ basic programs/Linkedlist/countNodes.cpp
@@ -0,0 +1,65 @@
+//write a program to count number of nodes in linkedlist//
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+}*first=NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct node *t, *last;
+    first=(struct node*)malloc(sizeof(struct node));
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+    
+    for(i=1;i<n;i++)
+    {
+        t=(struct node*)malloc(sizeof(struct node));
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+
+void display(struct node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d \n",p->data);
+        p=p->next;
+    }
+}
+
+void rDisplay(struct node *p)
+{
+  if(p!=NULL)
+  {
+    printf(" %d ",p->data);
+    rDisplay(p->next);
+  }
+}
+
+int count(struct node *p)
+{
+    int l=0;
+    while(p)
+    {
+        l++;
+        p=p->next;
+    }
+    return l;
+}
+int main()
+{
+    int A[]={3,4,5,6,7,8 };
+    create(A,5);
+    printf("length is %d\n",count(first));
+    return 0;
+    
+}
