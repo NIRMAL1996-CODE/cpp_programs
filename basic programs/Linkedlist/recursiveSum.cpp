6f997697b796218c3913cc70d0f5b77efc9afb09
NIRMAL_JAISWAL <nirmaljaiswal888@gmail.com>
2024-06-07 23:44:39 +0530

    write a program to find the recursive sum of elements in linkedlist

---
+++ basic programs/Linkedlist/recursiveSum.cpp
@@ -0,0 +1,93 @@
+//write a program to find the recursive sum of elements in linkedlist//
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+}*first=NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct node *t, *last;
+    first=(struct node*)malloc(sizeof(struct node));
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+    
+    for(i=1;i<n;i++)
+    {
+        t=(struct node*)malloc(sizeof(struct node));
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+
+void display(struct node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d \n",p->data);
+        p=p->next;
+    }
+}
+
+void rDisplay(struct node *p)
+{
+  if(p!=NULL)
+  {
+    printf(" %d ",p->data);
+    rDisplay(p->next);
+  }
+}
+
+int count(struct node *p)
+{
+    int l=0;
+    while(p)
+    {
+        l++;
+        p=p->next;
+    }
+    return l;
+}
+
+int Rcount(struct node *p)
+{
+    if(p!=NULL)
+        return Rcount(p->next)+1;
+    else
+        return 0;
+}
+
+int sum(struct node *p)
+{
+    int s=0;
+    while(p!=NULL)
+    {
+        s+=p->data;
+        p=p->next;
+    }
+    return s;
+}
+int Rsum(struct node *p)
+{
+    if(p==NULL)
+        return 0;
+    else
+        return Rsum(p->next)+p->data;
+}
+
+
+int main()
+{
+    int A[]={3,4,5,6,7,8 };
+    create(A,5);
+    printf("sum is %d\n",Rsum(first));
+    return 0;
+    
+}
