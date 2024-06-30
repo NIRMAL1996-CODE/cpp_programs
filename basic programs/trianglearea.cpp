c7a11449fffc89bb3b146d6e4fb71deb9fe38351
nirmal <nirmaljaiswal888@gmail.com>
2024-06-30 13:32:44 +0530

    write a program to find the area of triangle

---
+++ basic programs/trianglearea.cpp
@@ -0,0 +1,12 @@
+//write a program to find the area of triangle//
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b,h,a;
+    cout<<"ENTER BASE AND HEIGHT \n";
+    cin>>b>>h;
+    a=(b*h)/2;
+    cout<<"AREA IS "<<a;
+    return 0;
+}
